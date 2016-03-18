b0dd6949fc82ef1f4b65b620a0fede418a25dabe
Héctor Alejandro Valladolid Daumas <pica-angelous@hotmail.co>
2016-03-18 17:19:04 +0000

    Change background color

---
+++ views/buy_form.php
@@ -0,0 +1,13 @@
+<form action="buy.php" method="post">
+    <fieldset>
+        <div class="control-group">
+            <input style ="background-color: blue; color: black;"class="input-big" name="symbol" placeholder="Symbol" type="text"/>
+        </div>
+        <div class="control-group">
+            <input style ="background-color: yellow; color: black;" class="input-big" name="shares" placeholder="Shares" type="text"/>
+        </div>
+        <div class="control-group">
+            <button type="submit" class="btn">Buy</button>
+        </div>
+    </fieldset>
+</form>
