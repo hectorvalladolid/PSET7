3039ddd77ac248a3e6398baf91f62bfb65f27f78
Héctor Alejandro Valladolid Daumas <pica-angelous@hotmail.co>
2016-03-18 17:35:01 +0000

    Submit a photo

--- views/buy_form.php
+++ views/buy_form.php
@@ -1,10 +1,11 @@
 <form action="buy.php" method="post">
     <fieldset>
+    <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR1yDMBDNJh6aMySYEbHRs2h0tphgI6Pum_T6JuBe4wiPiMpEM7ag" alt="You can buy here" class="img-circle" style="width:304px;height:228px;">
         <div class="control-group">
             <input style ="background-color: blue; color: black;"class="input-big" name="symbol" placeholder="Symbol" type="text"/>
         </div>
         <div class="control-group">
-            <input style ="background-color: yellow; color: black;" class="input-big" name="shares" placeholder="Shares" type="text"/>
+            <input style ="background-color: blue; color: black;" class="input-big" name="shares" placeholder="Shares" type="text"/>
         </div>
         <div class="control-group">
             <button type="submit" class="btn">Buy</button>
